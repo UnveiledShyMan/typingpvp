52dbb84f6574698f1cbf10ab9e327f5cb5721863
UnveiledShyMan <kriocod@gmail.com>
2026-01-05 02:22:58 +0100

    Fix: Add missing existsSync import in server/index.js
    - Import existsSync from 'fs' module to fix production server errors
    - This was causing the server to crash when trying to check if client/dist exists

--- server/index.js
+++ server/index.js
@@ -5,6 +5,7 @@ import cors from 'cors';
 import { nanoid } from 'nanoid';
 import { fileURLToPath } from 'url';
 import { dirname, join } from 'path';
+import { existsSync } from 'fs';
 import authRoutes from './routes/auth.js';
 import userRoutes from './routes/users.js';
 import rankingsRoutes from './routes/rankings.js';
